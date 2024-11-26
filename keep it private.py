131d84217faf5136d95d227c8f28addf65a04188
bhavesh1234785 <ganesunibhavesh8@gmail.com>
2024-11-26 20:22:29 +0530

    keep it private

---
+++ keep it private.py
@@ -0,0 +1,17 @@
+#class creation
+class myClass:
+    #private variable
+    __privateVar=27;
+
+    #private method
+    def __privMeth(self):
+        print("I'm inside class myClass") 
+    
+    #function to print value of private variable
+    def hello(self):
+        print("private variable value:",myClass.__privateVar)
+#object creation and method call
+foo = myClass()
+foo.hello()
+foo.__privMeth
+           
